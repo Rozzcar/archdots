4b3aecc1e20002db1ae5872c9532995f434f0d0c
Rozzcar <ethanrobertallen@gmail.com>
2025-11-08 18:13:26 +0000

    Add files via upload

--- .config/hypr/scripts/random_wallpaper.sh
+++ .config/hypr/scripts/random_wallpaper.sh
@@ -1,1 +1,6 @@
+\#!/bin/bash
 
+WALLPAPER_DIR="$HOME/.config/wallpapers"
+RANDOM_WALLPAPER=$(find "$WALLPAPER_DIR" -type f | shuf -n 1)
+wal -i "$RANDOM_WALLPAPER"
+swww img  --transition-type center --transition-duration 1 --transition-fps 240 "$RANDOM_WALLPAPER"
